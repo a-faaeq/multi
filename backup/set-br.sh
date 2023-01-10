b4f6551468f35ec21550a4129c73d52f9b5902fc
sreyaeve <121951124+sreyaeve@users.noreply.github.com>
2023-01-10 22:23:06 +0700

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 # Link Hosting Kalian
-zenhost="raw.githubusercontent.com/sreyaeve/multi/backup/main/backup"
+zenhost="raw.githubusercontent.com/sreyaeve/multi/main/backup"
 
 apt install rclone -y
 printf "q\n" | rclone config
